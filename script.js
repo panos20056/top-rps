52848f3e725851a832a173363876397b4b588b55
panos <panosstavro@hotmail.com>
2022-10-13 16:46:28 +0300

    Add UI

--- script.js
+++ script.js
@@ -1,101 +1,80 @@
-const choices = ['rock', 'paper', 'scissors'];
-const patterns = /^(rock|paper|scissors)$/i
+const choices = ['Rock', 'Paper', 'Scissors'];
+// const patterns = /^(rock|paper|scissors)$/i;
+const buttons = document.querySelectorAll('.choice');
+const main = document.querySelector('main');
+const texts = document.querySelector('#texts');
+const selectionText = document.createElement('p');
+const userCurrentScoreText = document.querySelector('#userCurrentScore');
+const compCurrentScoreText = document.querySelector('#compCurrentScore');
+const resultPara = document.createElement('p');
+let userScore = 0;
+let compScore = 0;
+buttons.forEach(button => button.addEventListener('click', game));
 
-function checkInput(regex, text) {
-    return regex.test(text);    
-}
 
-function playerChoice() {
-    let playerInput = prompt('Weapon of choice');
-    if (checkInput(patterns, playerInput)) {
-        playerInput = playerInput.toLowerCase();
-        return playerInput;
-    } else if (playerInput === null) {
-        return null;
-    } else  {    
-        return playerChoice();
-    }    
-}
 
 function computerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function capFirstLetter(text) {
-    if (text === null) { return null; }
-    return text.charAt(0).toUpperCase() + text.slice(1);
-}
 
 // plays a single round of rock paper scissors
-// cancels the round if player choice is empty or cancelled
 // announces both player and computer selections and compares them
 // returns 0 for tie, 1 for player victory, 2 for computer victory and announces round result
-function playRound() {
+function playRound(buttonChoice) {
     let compSelection = computerChoice();
-    let playerSelection = playerChoice();
-    
-    if (playerSelection === null) {
-        return;
+    let roundValue;
+    selectionText.textContent = `${buttonChoice} vs ${compSelection}!`;
+    texts.appendChild(selectionText);
+    if ( compSelection == buttonChoice) {
+        resultPara.textContent = 'tie';
+        roundValue = 0;
+    } else if (buttonChoice === 'Rock' && compSelection === 'Scissors' || 
+                buttonChoice === 'Paper' && compSelection === 'Rock' || 
+                buttonChoice === 'Scissors' && compSelection === 'Paper') {
+        resultPara.textContent = `Round won! ${buttonChoice} beats ${compSelection}`;
+        roundValue = 1;
     } else {
-        playerSelection = capFirstLetter(playerSelection);
-        compSelection = capFirstLetter(compSelection);
-        console.log(`${playerSelection} vs ${compSelection}!`);
-        if ( compSelection == playerSelection) {
-        console.log('tie');
-        return 0;
-        } else if (playerSelection === 'rock' && compSelection === 'scissors' || playerSelection === 'paper' && compSelection === 'rock' || playerSelection === 'scissors' && compSelection === 'paper') {
-            console.log(`Round won! ${playerSelection} beats ${compSelection}`);
-            return 1;
-        } else {
-            console.log(`Round lost. `);
-            return 2;
-        }
+         resultPara.textContent = `Round lost. `;
+        roundValue = 2;
+    }
+    texts.appendChild(resultPara);
+    return roundValue;
+}
+
+function game(e) {
+    let playerChoice = e.target.textContent;
+    let roundResult = playRound(playerChoice);
+    if (roundResult == 1) {userScore++;}
+    else if (roundResult == 2) {compScore++;}
+    userCurrentScoreText.textContent = userScore + ' ';
+    compCurrentScoreText.textContent = compScore; 
+    if (userScore == 5) {
+        resultPara.textContent = 'Congratulations! You won the game!'
+        buttons.forEach(button => button.removeEventListener('click', game));
+        newGame();
+    } else if (compScore == 5) {
+        resultPara.textContent = 'You lose! Git gud' 
+        buttons.forEach(button => button.removeEventListener('click', game));
+        newGame();
     }
 }
 
-
-function countScore(score1, score2) {
-    return score1 == score2 ? 'Tie' :
-    score1 > score2 ? 'You win!' :
-    'You lose.' ;
-
+function newGame() {
+    const newGameButton = document.createElement('button');
+    newGameButton.textContent = 'New game';
+    texts.appendChild(newGameButton);
+    newGameButton.addEventListener('click', () => {
+        userScore = 0;
+        compScore = 0;
+        userCurrentScoreText.textContent = userScore;
+        compCurrentScoreText.textContent = compScore;
+        resultPara.textContent = '';
+        selectionText.textContent = '';
+        buttons.forEach(button => button.addEventListener('click', game));
+        texts.removeChild(newGameButton);
+    })
+    
 }
 
-//calls the playround() up to 5 times
-/* increases user score or comuter score by 1 each time one of them wins, leaves them 
-the same if its' a tie 
-
-if playround() returns undefined (which means prompt was cancelled) cancels loop
-and returns game canceled message, using the cancelled variable
-
-if loop is completed, announces the winner or tie*/
-//
-
-function playGame() {
-    let userScore = 0;
-    let compScore = 0;
-    let cancelled = false;
-    for (let i = 1; i < 6; i++){
-        if (Math.abs(userScore - compScore) <= 6 - i) {
-            console.log('Round ', i)
-            let roundResult = playRound();
-            if (roundResult == 1) {
-                userScore += 1;
-            } else if (roundResult == 2) {
-                compScore += 1;
-            } else if (roundResult === undefined) {
-                cancelled = true;
-                break;
-            } else {
-                continue;
-            }
-        }
-        console.log(userScore, compScore); 
-        
-    }
-    if (cancelled) {
-        return 'You cancelled the game'
-    }
-    return countScore(userScore, compScore);
-}
-
+ 
